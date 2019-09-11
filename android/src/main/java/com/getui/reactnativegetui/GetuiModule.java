7d35628ae94a8983de517e6f80a1c33057833bb1
shizhenzhen <kumbaya123@126.com>
2019-09-11 19:58:35 +0800

    compile更改为implementation，support包更改为androidx

--- android/src/main/java/com/getui/reactnativegetui/GetuiModule.java
+++ android/src/main/java/com/getui/reactnativegetui/GetuiModule.java
@@ -3,7 +3,8 @@ package com.getui.reactnativegetui;
 import android.content.Context;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
+
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
